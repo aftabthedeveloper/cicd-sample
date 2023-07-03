918b62a7e7a757a202daf41fde8b208ece3676a6
Aftab Alam <mailaftab786@gmail.com>
2023-07-04 00:43:20 +0545

    Initial Commit

---
+++ src/main/java/com/aftabthedeveloper/projects/cicdsample/CicdsampleApplication.java
@@ -0,0 +1,13 @@
+package com.aftabthedeveloper.projects.cicdsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdsampleApplication.class, args);
+	}
+
+}
